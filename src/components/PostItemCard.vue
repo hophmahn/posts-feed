78bf326f04e047884fa30182600f105d758cb230
Dastan Ahmet <dastanahmet01@gmail.com>
2019-10-29 02:31:45 +0600

    Add post item card

---
+++ src/components/PostItemCard.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="post-item">
+    <router-link to="/">Go back to Post List</router-link>
+    <h1 class="title">{{ post && post.title }}</h1>
+    <h5 class="author">{{ post && post.user && post.user.name }}</h5>
+    <div class="body">{{ post && post.body }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    post: {
+      title: String,
+      user: Object,
+      body: String
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.post-item {
+  border: 1px solid rgba(100, 100, 100, 0.3);
+  border-radius: 4px;
+  box-shadow: 1px 1px 10px rgba(0, 0, 0, 0.1);
+  padding: 20px;
+  margin-bottom: 20px;
+
+  .body {
+    text-align: justify;
+  }
+}
+</style>
